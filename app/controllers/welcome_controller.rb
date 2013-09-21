b922f35d66e0cd573e9c7c29b1ea2d3a66177e21
ChrisH <childebrand@shfl.com>
2013-09-21 08:14:07 -0700

    Add new paths, default to randomly capitalize

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,72 @@
 class WelcomeController < ApplicationController
 
-  def index
-    words = %w(a I to is you my like in and that of his from one by as have or had were every think first ride going walk well stop came why it the he was for on are with they go when an how if out your which their about many play please look ask may must best off tell into at be this but not what all we can said them then some would him two been long down her these find read right round say now story could jump there she do will up so make has see no day know very just much new give after man before let live under use away want soon call open eat who did get come little me our good am yes old boy put us work any three here again take family pretty funny ate saw help went where thank over)
+  @@words = %w(a I to is you my like in and that of his from one by as have or had were every think first ride going walk well stop came why it the he was for on are with they go when an how if out your which their about many play please look ask may must best off tell into at be this but not what all we can said them then some would him two been long down her these find read right round say now story could jump there she do will up so make has see no day know very just much new give after man before let live under use away want soon call open eat who did get come little me our good am yes old boy put us work any three here again take family pretty funny ate saw help went where thank over)
 
-    words = words.shuffle
+  def index
+    words = @@words.shuffle
+
+    @words = []
+
+    counter = 0
+    row = 0
+    words.each do |word|
+      @words[row] ||= []
+
+      if Random.rand(2) == 1
+        @words[row] << word.capitalize
+      else
+        @words[row] << word
+      end
+
+      counter += 1
+      if counter >= 10
+        counter = 0
+        row += 1
+      end
+    end
+  end
+
+
+  def capitalize
+    words = @@words.shuffle
+
+    @words = []
+
+    counter = 0
+    row = 0
+    words.each do |word|
+      @words[row] ||= []
+      @words[row] << word.capitalize
+      counter += 1
+      if counter >= 10
+        counter = 0
+        row += 1
+      end
+    end
+    render :index
+  end
+
+  def upper
+    words = @@words.shuffle
+
+    @words = []
+
+    counter = 0
+    row = 0
+    words.each do |word|
+      @words[row] ||= []
+      @words[row] << word.upcase
+      counter += 1
+      if counter >= 10
+        counter = 0
+        row += 1
+      end
+    end
+    render :index
+  end
+
+  def lower
+    words = @@words.shuffle
 
     @words = []
 
@@ -18,6 +81,8 @@ class WelcomeController < ApplicationController
         row += 1
       end
     end
-
+    render :index
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Sightwords::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
-
+  match '/lower' => 'welcome#lower'
+  match '/upper' => 'welcome#upper'
+  match '/capitalize' => 'welcome#capitalize'
 end
